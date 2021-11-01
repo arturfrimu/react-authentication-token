b6b6403a1c6fc9787d6bd92b5125ef966b8943fb
frimuartur <artur.frimu2@gmail.com>
2021-11-01 23:25:36 +0200

    306

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,14 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useContext} from 'react';
 
 import classes from './AuthForm.module.css';
+import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
+
+    const authCtx = useContext(AuthContext);
+
     const [isLogin, setIsLogin] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -51,7 +55,7 @@ const AuthForm = () => {
                     })
                 }
             }).then(data => {
-                console.log(data)
+                authCtx.login(data.idToken);
             })
                 .catch((err) => {
                     alert(err.message);
