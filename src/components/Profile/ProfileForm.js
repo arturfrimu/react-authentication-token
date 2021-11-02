65108e0a797e00b13b14ad034f05159d9380c13f
frimuartur <artur.frimu2@gmail.com>
2021-11-02 08:45:40 +0200

    307

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,48 @@
+import React, {useRef, useContext} from "react";
 import classes from './ProfileForm.module.css';
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+    const newPasswordInputRef = useRef();
+    const authCtx = useContext(AuthContext);
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredNewPassword = newPasswordInputRef.current.value;
+
+        // validation
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDEzrKBokDPpQAhXSIOI2rvGonRPn_VDEs', {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: authCtx.token,
+                password: enteredNewPassword,
+                returnSecureToken: false
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if (res.ok) {
+                console.log('OK')
+            } else {
+                console.log('ELSE')
+            }
+        })
+    }
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor='new-password'>New Password</label>
+                <input ref={newPasswordInputRef} minLength="7" type='password' id='new-password'/>
+            </div>
+            <div className={classes.action}>
+                <button>Change Password</button>
+            </div>
+        </form>
+    );
 }
 
 export default ProfileForm;
