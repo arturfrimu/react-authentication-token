3def6a22891f8c2c2cb03874ef34e8d57fc4b114
frimuartur <artur.frimu2@gmail.com>
2021-11-02 09:45:29 +0200

    312

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -9,20 +9,32 @@ const AuthContext = React.createContext({
     }
 });
 
+const calculateRemainingTime = (expirationTime) => {
+    const currentTime = new Date().getTime();
+    const adjExpirationTime = new Date(expirationTime).getTime();
+
+    return adjExpirationTime - currentTime; // remaining duration
+}
+
 export const AuthContextProvider = (props) => {
     const initialToken = localStorage.getItem('token');
     const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token;
 
-    const loginHandler = (token) => {
-        setToken(token);
-        localStorage.setItem('token', token);
-    }
-
     const logoutHandler = () => {
         setToken(null);
         localStorage.removeItem('token');
+    }
+
+    const loginHandler = (token, expirationTime) => {
+        setToken(token);
+        localStorage.setItem('token', token);
+
+        const remainingTime = calculateRemainingTime();
+
+        setTimeout(logoutHandler, remainingTime)
+        // setTimeout(logoutHandler, 3000)
     }
 
     const contextValue = {
